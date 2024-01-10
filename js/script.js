814d09eecc30950294973459b56c3b212b482828
LucaB98 <lucabellan98@gmail.com>
2024-01-10 17:38:32 +0100

    fix css for body and add function js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('JS OK')
+
+
+// PRENDO GLI ELEMENTI CHE MI SERVONO
+const formElement = document.querySelector('form');
+const grid = document.getElementById('grid');
+
+// funzioni
+
+const createCell = (number) => {
+    const cell = document.createElement('div');
+    cell.classList = 'cell'
+    return;
+};
+
+
+
+const rows = 10
+const cols = 10
+const totCell = rows * cols
+
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    for(let i = 0; i < totCell; i++){
+        const cell = createCell(i);
+        grid.appendChild(cell);
+    }
+});
