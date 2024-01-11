49bd7c18a4972e6b736b99ef2d7f1c9231b542de
LucaB98 <lucabellan98@gmail.com>
2024-01-11 11:31:10 +0100

    added score and bombs

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 // PRENDO GLI ELEMENTI CHE MI SERVONO
 const formElement = document.querySelector('form');
 const grid = document.getElementById('grid');
+const select = document.getElementById('select');
+const scoreDisplay = document.getElementById('score');
 
 // funzioni
 
@@ -10,20 +12,55 @@ const createCell = (content) => {
     const newCell = document.createElement('div');
     newCell.classList = 'cell'
     newCell.innerText = content;
+
     return newCell;
 };
 
+const generateBombs = (maxBombNumber, totalBombs) => {
+    const bombs = [];
+    while(bombs.length < totalBombs){
+        const randomNumber = Math.floor(Math.random()* maxBombNumber) + 1;
+        if(!bombs.includes(randomNumber)) bombs.push(randomNumber);
+    }
+    return bombs;
+}
 
 
-const rows = 10;
-const cols = 10;
-const totCell = rows * cols;
+
 
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
 
     grid.innerHTML = '';
+
+    const level = select.value;
+
+    let rows = 10;
+    let cols = 10;
+
+    switch(level){
+        case 'difficile':
+            rows = 7;
+            cols = 7;
+            break;
+        case 'normale':
+            rows = 9;
+            cols = 9;
+            break;    
+    };
+
+    const root = document.querySelector(':root');
+    root.style.setProperty('--cols-per-row', cols);
+
+    const totCell = rows * cols;
+
+    let score = 0;
+    scoreDisplay.innerText = score;
+
+    const totalBombs = 16;
+    const maxPoint = totCell - totalBombs;
+    const bombs = generateBombs(totCell, totalBombs)
 
 
     for(let i = 1; i <= totCell; i++){
@@ -32,11 +69,14 @@ formElement.addEventListener('submit', (e) => {
 
         cell.addEventListener('click', () => {
 
+            if(cell.classList.contains('clicked')) return;
             const cellValue = cell.innerText;
             console.log('Valore della casella cliccata:', cellValue);
             
-            cell.classList.toggle('clicked');
-            
+
+            cell.classList.add('clicked');
+    
+            scoreDisplay.innerText = ++score;
         });
 
         grid.appendChild(cell);
